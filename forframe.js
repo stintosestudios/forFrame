e1c9d90cb7040d0ddb4bebf76ed98d07a7420371
stintosestudios <stintose.studios@gmail.com>
2016-12-07 15:10:17 -0500

    first commit

---
+++ forframe.js
@@ -0,0 +1,151 @@
+
+var scene = (function () {
+
+    var state = {
+
+        frame : 0,
+        maxFrame : 50,
+        percentDone : 0,
+        forFrame : null,
+        parts : [],
+        canvas : null,
+        ctx : null
+
+    };
+
+    // the Part Class.
+    var Part = function (values) {
+
+        this.id = values.id;
+        this.w = values.w;
+        this.h = values.h;
+        this.x = 0;
+        this.y = 0;
+        this.radian = 0;
+
+        console.log('new part');
+
+    };
+
+    // main scene setup method
+    var api = function (options) {
+
+        // no state?
+        if (options == undefined) {
+
+            throw new Error('you need to give a state object, see the README');
+
+        }
+
+        // no parts?
+        if (options.parts === undefined) {
+
+            throw new Error('you must define at least one Part for your scene, see the README');
+
+        }
+
+        // no forFrame?
+        if (options.forFrame === undefined) {
+
+            throw new Error('you must define a forFrame method, see the README.');
+
+        }
+
+        // setup parts array.
+        state.parts = {};
+        options.parts.forEach(function (currentPart) {
+
+            state.parts[currentPart.id] = new Part(currentPart);
+
+        });
+
+        state.forFrame = options.forFrame;
+
+    };
+
+    // inject a canvas into the given id
+    api.injectCanvas = function (id) {
+
+        state.canvas = document.createElement('canvas');
+        state.ctx = state.canvas.getContext('2d');
+
+        state.canvas.width = 640;
+        state.canvas.height = 480;
+
+        state.ctx.fillStyle = 'black';
+        state.ctx.fillRect(0, 0, state.canvas.width, state.canvas.height);
+
+        document.getElementById(id).appendChild(state.canvas);
+
+    };
+
+    // render the current frame
+    api.renderFrame = function () {
+
+        var prop,
+        pt,
+        ctx = state.ctx;
+
+        // clear canvas.
+        state.ctx.fillStyle = 'black';
+        state.ctx.fillRect(0, 0, state.canvas.width, state.canvas.height);
+
+        // ALERT! a for in loop!? NO!
+        for (prop in state.parts) {
+
+            pt = state.parts[prop];
+
+            ctx.strokeStyle = '#ffffff';
+
+            ctx.strokeRect(pt.x, pt.y, pt.w, pt.h);
+
+        }
+
+    };
+
+    // set animation to the given frame.
+    api.setFrame = function (frameNum) {
+
+        state.frame = frameNum;
+
+        if (state.frame === -1) {
+            state.frame = state.maxFrame - 1;
+        }
+        if (state.frame === state.maxFrame) {
+            state.frame = 0;
+        }
+
+        // call the forFrame method;
+        state.forFrame.call(state);
+
+        state.percentDone = state.frame / state.maxFrame;
+
+    };
+
+    // step the current frame
+    api.step = function (back) {
+
+        var rate = back ? -1 : 1;
+
+        state.frame += rate;
+        api.setFrame(state.frame);
+
+
+    };
+
+    // play the scene
+    api.play = function () {
+
+        // ALERT! setTimeout? what?
+        setTimeout(api.play, 33);
+
+        api.renderFrame();
+
+        api.step();
+
+    };
+
+    return api;
+
+}
+    ());
