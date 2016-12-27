5e5f06c078d7356324fadc14efb38536850777d4
stintosestudios <stintose.studios@gmail.com>
2016-12-27 14:46:27 -0500

    new viewport demo

---
+++ demo/viewport.js
@@ -0,0 +1,96 @@
+
+
+scene({
+
+    maxFrame : 50,
+
+    opacity : 0.8,
+
+    logo : {
+        w : 128,
+        h : 56,
+        x : 512,
+        y : 424,
+        opacity : 0.3,
+        skin : {
+            imgIndex : 1,
+            sx : 0,
+            sy : 0,
+            sw : 128,
+            sh : 56
+        }
+    },
+
+    // define some parts
+    parts : [{
+            id : 'maintext',
+            w : 128,
+            h : 32,
+            opacity : 1,
+            skin : {
+
+                //renderPartBox : true,
+                imgIndex : 0,
+                sw : 128,
+                sh : 32,
+                appendRender : function (ctx, skin) {
+
+                    ctx.fillStyle = 'rgba(0,0,0,0.5)';
+                    ctx.fillRect(0, 0, skin.part.w, skin.part.h);
+
+                }
+
+            }
+
+        }
+
+    ],
+
+    forFrame : function () {
+
+        var f = 1,
+        pt,
+        radian,
+        dRadian = Math.PI * 2 / 3,
+        bias = Math.abs((0.5 - this.percentDone) / 0.5);
+
+        pt = this.parts['maintext'];
+        pt.w = 256 + 256 * bias;
+        pt.h = 32 + 128 * bias;
+        pt.opacity = bias;
+        pt.x = 320 - (pt.w / 2);
+        pt.y = 240 - (pt.h / 2);
+
+    }
+
+});
+
+// inject a canvas into an element with an id of 'apparea'.
+scene.injectCanvas('apparea');
+
+scene.load(
+    [
+        'demo/img/logo.png',
+        'demo/img/mylogo_128.png'
+    ],
+    function (progress) {
+
+    // playback object
+    var playback = {
+        appendRender : function (ctx) {
+
+            ctx.fillStyle = '#ff0000';
+            ctx.fillRect(0, 0, this.viewPort.w / 2, this.viewPort.h 	/ 2);
+
+        },
+        containerId : 'apparea',
+        appendZ : 0,
+        frameRate : 24
+    };
+
+    if (progress === 1) {
+        scene.injectUI(playback);
+
+    }
+
+});
